bb9b22f8d03927a1e47017cac466e622ea455f43
shahid <shahidzhahidmohammed@gmail.com>
2026-01-12 19:04:28 +0530

    google credential

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,14 +11,10 @@ app.use(cors({
     credentials:true
 }))
 
-
 app.use(express.json({limit:"16kb"}))  //we accept json data
 // app.use(express.urlencoded())       data is from url's
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-
-
-
 
 export {app}
---
+++ Backend/src/service/google.service.js
@@ -0,0 +1,9 @@
+import {OAuth2Client} from "google-auth-library"
+import dotenv from "dotenv"
+
+dotenv.config({path:"./.env"})
+
+const googleclient=new OAuth2Client(process.dotenv.client_id);
+
+
+
