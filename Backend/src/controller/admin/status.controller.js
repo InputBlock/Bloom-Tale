2727a29e366a7f6e646a56f3d5f9125ea4ef6cdd
shahid <shahidzhahidmohammed@gmail.com>
2026-01-15 17:22:21 +0530

    s

---
+++ Backend/src/controller/admin/status.controller.js
@@ -0,0 +1,12 @@
+import {getUserInfo} from "../../admin/status.js"
+
+const get_user_info=async(req , res)=>{
+    try{
+      const data=await getUserInfo();
+
+      return res.status(200).json(data);
+    }catch(err){
+        return res.status(500).json({error:err.message});
+    }   
+
+}
